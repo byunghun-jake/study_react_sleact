ad2b6801ceedabfd90ecab6af0b35973048e36a7
byunghun-jake <figma@kakao.com>
2021-11-01 12:53:01 +0900

    config: package, eslint, prettier, ts, webpack

---
+++ alecture/client.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { render } from "react-dom"
+// import App from "./layouts/App"
+
+const App = () => {
+  return <h1>Hello!! world!</h1>
+}
+
+render(<App />, document.querySelector("#app"))
