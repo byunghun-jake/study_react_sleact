8c0c708220ad1187c9f7025fcfcd9a42851ceec4
byunghun-jake <figma@kakao.com>
2021-11-03 15:42:04 +0900

    package: 절대경로, emotion

--- alecture/client.tsx
+++ alecture/client.tsx
@@ -1,9 +1,5 @@
 import React from "react"
 import { render } from "react-dom"
-// import App from "./layouts/App"
-
-const App = () => {
-  return <h1>Hello!! world! LOL!</h1>
-}
+import App from "@layouts/App"
 
 render(<App />, document.querySelector("#app"))
